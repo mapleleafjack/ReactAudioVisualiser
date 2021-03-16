edcd8130ddea9151d9f178371383f94e0231de34
mapleleafjack <mapleleafjack@gmail.com>
2021-03-16 13:01:13 +0000

    now they're three(.js)

--- src/Audio3DVisualiser.js
+++ src/Audio3DVisualiser.js
@@ -11,23 +11,40 @@ class Audio3DVisualiser extends Component {
       const { audioData } = this.props;
 
 
+      let low = (audioData[20] / 128.0);
+      let med = (audioData[500] / 128.0);
+      let hi = (audioData[1000] / 128.0);
 
+
+
+      // let low_array = audioData.slice(0, 300);
+      // let medium_array = audioData.slice(300, 700);
+      // let high_array = audioData.slice(700, 1024);
+      //
+      // let low = low_array.reduce((a, b) => a + b, 0);
+      // let avg_avg = ((low / low_array.length) || 0) / 128;
+      //
+      // let med = medium_array.reduce((a, b) => a + b, 0);
+      // let med_avg = ((med / medium_array.length) || 0) / 128;
+      //
+      // let hi = high_array.reduce((a, b) => a + b, 0);
+      // let hi_avg = ((hi / high_array.length) || 0) / 128;
 
       return <Canvas>
-        <Cube audioData={audioData}/>
+          <mesh position={[-2,0,0]} scale={[1, low, 1]}>
+            <boxBufferGeometry attach="geometry" />
+            <meshBasicMaterial attach="material" color="red" opacity={0.5} transparent />
+          </mesh>
+          <mesh position={[0,0,0]} scale={[1, med, 1]}>
+            <boxBufferGeometry attach="geometry" />
+            <meshBasicMaterial attach="material" color="orange" opacity={0.5} transparent />
+          </mesh>
+          <mesh position={[2,0,0]} scale={[1, hi, 1]}>
+            <boxBufferGeometry attach="geometry" />
+            <meshBasicMaterial attach="material" color="blue" opacity={0.5} transparent />
+          </mesh>
       </Canvas>
     }
-}
-
-const Cube = (props) => {
-  let low = (props.audioData[20] / 128.0);
-  let med = (props.audioData[500] / 128.0);
-  let hi = (props.audioData[500] / 128.0);
-
-  return <mesh position={[0,0,0]} scale={[1,med,hi]}>
-      <boxBufferGeometry attach="geometry" />
-      <meshBasicMaterial attach="material" color="blue" opacity={0.5} transparent />
-    </mesh>
 }
 
 
